8c174c8173a39b1a46ab58b4bd589a629c2301bf
chee <chee@snoot.club>
2022-07-21 21:59:36 -0500

    make whole interface public

--- src/main.zig
+++ src/main.zig
@@ -3,23 +3,17 @@ const std = @import("std");
 const consonants = [_]u8{ 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
 const vowels = [_]u8{ 'a', 'e', 'i', 'o', 'u' };
 
-fn consonant() u8 {
+pub fn consonant() u8 {
     return consonants[std.crypto.random.int(u8) % consonants.len];
 }
 
-fn vowel() u8 {
+pub fn vowel() u8 {
     return vowels[std.crypto.random.int(u8) % vowels.len];
 }
 
-fn word() std.fmt.BufPrintError![5]u8 {
+pub fn word() std.fmt.BufPrintError![5]u8 {
     var w: [5]u8 = undefined;
-    _ = try std.fmt.bufPrint(&w, "{c}{c}{c}{c}{c}", .{
-        consonant(),
-        vowel(),
-        consonant(),
-        vowel(),
-        consonant()
-    });
+    _ = try std.fmt.bufPrint(&w, "{c}{c}{c}{c}{c}", .{ consonant(), vowel(), consonant(), vowel(), consonant() });
     return w;
 }
 
